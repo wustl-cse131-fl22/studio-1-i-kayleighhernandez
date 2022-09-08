3603bf97a95fb7e673c80baa4ea6022d377aa123
kayleighhernandez <kayleighhernandez@10.232.129.108>
2022-09-08 12:23:08 -0500

    studio 1 kayleigh hernandez september 8

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -7,16 +7,22 @@ public class HiOne {
 	/**
 	 * Says hello to the supplied argument
 	 */
-	public static void main(String[] args) {
+	 
+		
+		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
 
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
+		System.out.println("Whats your name?");
+		String name = in.nextLine ();
+		System.out.println("Hi " + name );
+		System.out.println( "How are you " );
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
 		//
+		
+		
 
 	}
 
